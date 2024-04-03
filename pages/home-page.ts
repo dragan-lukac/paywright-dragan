6893621775d4794441cb70242ca5c2d2447417c6
Dragan Lukac <dragan.lukac@gmnail.com>
2024-04-03 20:58:57 +0200

    add HomePage refactor in login-tests to use HomePage

---
+++ pages/home-page.ts
@@ -0,0 +1,37 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class HomePage {
+    readonly page: Page;
+    readonly loginHeaderButton: Locator;
+    readonly usernameField: Locator;
+    readonly passwordField: Locator;
+    readonly loginWindowButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.loginHeaderButton = page.getByRole("link", { name: "Log in" });
+        this.usernameField = page.locator("#loginusername");
+        this.passwordField = page.locator("#loginpassword");
+        this.loginWindowButton = page.getByRole("button", { name: "Log in" });
+    }
+
+    async navigateToHomePage() {
+        await this.page.goto('https://www.demoblaze.com/');
+    }
+
+
+    async clickOnHeaderLoginButton() {
+        await this.loginHeaderButton.click();
+    }
+
+    async insertCredentials(username: string, password: string) {
+        await this.usernameField.clear();
+        await this.usernameField.fill(username);
+        await this.passwordField.clear();
+        await this.passwordField.fill(password);
+    }
+
+    async clickOnLoginWindowButton() {
+        await this.loginWindowButton.click();
+    }
+}
--- tests/ui/login-test.spec.ts
+++ tests/ui/login-test.spec.ts
@@ -1,33 +1,22 @@
 import { test, expect } from "@playwright/test";
+import { HomePage } from "../../pages/home-page";
 
 test.describe("Login tests", () => {
-test("Login with valid credentials", async ({ page }) => {
-    await page.goto("https://www.demoblaze.com/");
-    await page.getByRole("link", { name: "Log in" }).click();
-    await page.locator("#loginusername").click();
-    await page.locator("#loginusername").fill("username");
-    await page.locator("#loginusername").press("Tab");
-    await page.locator("#loginpassword").fill("password");
-    await page.getByRole("button", { name: "Log in" }).click();
-    await expect(
-      page.getByRole("link", { name: "Log out" })
-    ).toBeVisible();
+  test.only("Login with valid credentials", async ({ page }) => {
+    const homePage = new HomePage(page);
+    homePage.navigateToHomePage();
+    homePage.clickOnHeaderLoginButton();
+    homePage.insertCredentials("username", "password");
+    homePage.clickOnLoginWindowButton();
+    await expect(page.getByRole("link", { name: "Log out" })).toBeVisible();
   });
 
-  test("Login with invalid credentials", async ({ page }) => {
-    await page.goto("https://www.demoblaze.com/");
-    await page.getByRole("link", { name: "Log in" }).click();
-    await page.locator("#loginusername").click();
-    await page.locator("#loginusername").fill("username");
-    await page.locator("#loginusername").press("Tab");
-    await page.locator("#loginpassword").fill("invalidPassword");
-    await page.getByRole("button", { name: "Log in" }).click();
-    page.on('dialog', async dialog => {
-      console.log(dialog.message());
-      await dialog.accept();
-    });
-    await expect(
-      page.getByRole("link", { name: "Log in" })
-    ).toBeVisible();
+  test.only("Login with invalid credentials", async ({ page }) => {
+    const homePage = new HomePage(page);
+    homePage.navigateToHomePage();
+    homePage.clickOnHeaderLoginButton();
+    homePage.insertCredentials("invalidUsername", "invalidPassword");
+    homePage.clickOnLoginWindowButton();
+    await expect(page.getByRole("link", { name: "Log in" })).toBeVisible();
   });
 });
